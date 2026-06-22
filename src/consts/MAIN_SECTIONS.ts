098145ab1035ddd563c71dfc415911f2eda41bc1
Filipe Molina <filipemolina@live.com>
2026-06-22 11:11:30 -0500

    Preparing the change to Go and Bubble Tea

--- src/consts/MAIN_SECTIONS.ts
+++
@@ -1,5 +0,0 @@
-const MAIN_SECTIONS = ["Home", "Groups"] as const;
-
-type Section = (typeof MAIN_SECTIONS)[number];
-
-export { MAIN_SECTIONS, type Section };
--- src/utils/getEmptyConfigObject.ts
+++
@@ -1,8 +0,0 @@
-import type { DockerComposeFile } from "../types/common";
-
-export default function getEmptyConfigObject(): DockerComposeFile {
-  return {
-    name: "",
-    services: {},
-  };
-}
