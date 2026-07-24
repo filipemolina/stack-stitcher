ea7cd4e727cc71548bc00b8035de2ea6af34456a
stack-stitcher <dev@stack-stitcher.local>
2026-07-23 20:25:42 -0500

    Add cmds for compose-file creation

---
+++ src/cmds/CreateComposeFile.go
@@ -0,0 +1,20 @@
+package cmds
+
+import (
+	"stack-stitcher/src/utils"
+
+	tea "charm.land/bubbletea/v2"
+)
+
+type CreateComposeFileMsg struct {
+	Err error
+}
+
+// CreateComposeFile writes a brand-new compose file at fileName in the
+// current working directory, optionally pre-seeded with one service. Empty
+// serviceName/image means "create an empty services: mapping".
+func CreateComposeFile(fileName string, serviceName string, image string) tea.Cmd {
+	return func() tea.Msg {
+		return CreateComposeFileMsg{Err: utils.WriteNewComposeFile(fileName, serviceName, image)}
+	}
+}
